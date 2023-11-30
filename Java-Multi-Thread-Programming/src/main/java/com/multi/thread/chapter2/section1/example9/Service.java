a6f0d17e16d4fff163bce1a4ff4b8ee78e58517b
MaxQiu <yueyiqiu178@gmail.com>
2023-11-30 10:51:20 +0800

    修正service

--- Java-Multi-Thread-Programming/src/main/java/com/multi/thread/chapter2/section1/example9/Service.java
+++ Java-Multi-Thread-Programming/src/main/java/com/multi/thread/chapter2/section1/example9/Service.java
@@ -7,18 +7,22 @@ package com.multi.thread.chapter2.section1.example9;
  */
 public class Service {
 
-    public synchronized void testMethod() {
-        if (Thread.currentThread().getName().equals("A")) {
-            System.out.println("Thread name = " + Thread.currentThread().getName() + " run begintime = " + System.currentTimeMillis());
-            int i = 1;
-            while (i == 1) {
-                if (("" + Math.random()).substring(0, 8).equals("0.123456")) {
-                    System.out.println("ThreadName = " + Thread.currentThread().getName() + " run exceptionTime = " + System.currentTimeMillis());
-                    Integer.parseInt("a");
-                } else {
-                    System.out.println("Thread B run time = " + System.currentTimeMillis());
-                }
-            }
-        }
-    }
+	public synchronized void testMethod() {
+		if (Thread.currentThread().getName().equals("A")) {
+			System.out.println("Thread name = " + Thread.currentThread().getName() + " run begintime = "
+					+ System.currentTimeMillis());
+			int i = 1;
+			while (i == 1) {
+				if (("" + Math.random()).substring(0, 8).equals("0.123456")) {
+					System.out.println("ThreadName = " + Thread.currentThread().getName() + " run exceptionTime = "
+							+ System.currentTimeMillis());
+					Integer.parseInt("a");
+				} else {
+					System.out.println("Thread B run time = " + System.currentTimeMillis());
+				}
+			}
+		} else {
+			System.out.println("Thread B run Time=" + System.currentTimeMillis());
+		}
+	}
 }
