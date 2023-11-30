827396a85e5cb3ab7e2e0e7759a98e73cd02bb49
MaxQiu <yueyiqiu178@gmail.com>
2023-11-30 14:06:29 +0800

    修正Task.java

--- Java-Multi-Thread-Programming/src/main/java/com/multi/thread/chapter2/section2/example6/Task.java
+++ Java-Multi-Thread-Programming/src/main/java/com/multi/thread/chapter2/section2/example6/Task.java
@@ -7,11 +7,11 @@ package com.multi.thread.chapter2.section2.example6;
  */
 public class Task {
 
-	synchronized public void otherMethod() {
+	public void otherMethod() {
         System.out.println("----------------------run------------otherMethod");
     }
 
-    public synchronized void doLongTimeTask() {
+    public void doLongTimeTask() {
     	synchronized(this) {
 	        for (int i = 0; i < 10000; i++) {
 	            System.out.println("synchronized threadName = " + Thread.currentThread().getName() + " i = " + (i + 1));
